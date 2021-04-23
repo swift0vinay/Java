8fd9e0647be1286022982877d393c27ccd01c469
Vinay Jain <47448891+swift0vinay@users.noreply.github.com>
2021-04-23 12:53:31 +0530

    Update LowestCommonAncestor.java

--- LowestCommonAncestor.java
+++ LowestCommonAncestor.java
@@ -27,3 +27,17 @@
 			this.val = val;
 		}
 	}
+/* 
+	Algo-Alert
+	The approach is pretty intuitive. Traverse the tree in a depth first manner. The moment you encounter either of the nodes p or q, return some boolean flag. The flag helps to determine if we found the required nodes in any of the paths. The least common ancestor would then be the node for which both the subtree recursions return a True flag. It can also be the node which itself is one of p or q and for which one of the subtree recursions returns a True flag.
+
+	Let us look at the formal algorithm based on this idea.
+
+	Algorithm
+
+	Start traversing the tree from the root node.
+	If the current node itself is one of p or q, we would mark a variable mid as True and continue the search for the other node in the left and right branches.
+	If either of the left or the right branch returns True, this means one of the two nodes was found below.
+	If at any point in the traversal, any two of the three flags left, right or mid become True, this means we have found the lowest common ancestor for the nodes p and q.
+	Let us look at a sample tree and we search for the lowest common ancestor of two nodes 9 and 11 in the tree.
+*/
